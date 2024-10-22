7c589cee2820c6629dad07ed941d3b5f0969d433
Ridiculouswifi <thtan02@gmail.com>
2024-10-23 00:06:06 +0800

    Motor Control Functioning

--- freedom_board/robotBrain.h
+++ freedom_board/robotBrain.h
@@ -30,41 +30,49 @@ void handleCommand(int command) {
 		// Move Back
 		ledControl(YELLOW);
 		offLED();
-		motorControl(STRAIGHT, 25);
+		motorControl(REVERSE, 50);
 		
 	} else if (command == 0x08) {
 		// Turn Left on the spot
 		ledControl(PURPLE);
+		motorControl(TURN_ANTICLOCKWISE, 50);
 		
 	} else if (command == 0x04) {
 		// Turn Right on the spot
 		ledControl(PURPLE);
+		motorControl(TURN_CLOCKWISE, 90);
 		
 	} else if (command == 0x09) {
 		// Move Front and Turn Left
 		ledControl(SKY);
+		motorControl(FORWARD_LEFT_TURN, 90);
 		
 	} else if (command == 0x05) {
 		// Move Front and Turn Right
 		ledControl(SKY);
+		motorControl(FORWARD_RIGHT_TURN, 50);
 		
 	} else if (command == 0x0a) {
 		// Move Back and Turn Left
 		ledControl(SKY);
+		motorControl(BACKWARDS_LEFT_TURN, 50);
 		
 	} else if (command == 0x06) {
 		// Move Back and Turn Right
 		ledControl(SKY);
+		motorControl(BACKWARDS_RIGHT_TURN, 50);
 		
 	} else if (command == 0x00) {
 		// Stop
 		ledControl(RED);
 		onLED();
+		motorControl(10, 0);
 		
 	} else {
 		// Command is not recognised, discard command
 		ledControl(BLUE);
 		onLED();
+		motorControl(10, 0);
 	}
 }
 
--- freedom_board/robotMotorControl.h
+++ freedom_board/robotMotorControl.h
@@ -28,7 +28,7 @@ void initMotor() {
     SIM->SOPT2 |= SIM_SOPT2_TPMSRC(1); // Use MCGFLLCLK or MCGPLLCLK
 
     // Set the modulo value for PWM frequency (1 kHz)
-    TPM1->MOD = 11999; // Assuming 48MHz clock
+    TPM1->MOD = 4999; // Assuming 48MHz clock
 
     TPM1->SC &= ~((TPM_SC_CMOD_MASK) | (TPM_SC_PS_MASK));
     TPM1->SC |= (TPM_SC_CMOD(1) | TPM_SC_PS(2));
@@ -41,14 +41,14 @@ void initMotor() {
     PORTB->PCR[0] = PORT_PCR_MUX(3); // Set PTB0 to TPM0_CH0
     TPM1_C0SC &= ~((TPM_CnSC_ELSB_MASK) | (TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSA_MASK));
     TPM1_C0SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));
-    TPM1_C0V = 0;
+    TPM1_C0V = 1;
     
     // Configure PTB1 (Channel 1) for PWM
     PORTB->PCR[1] &= ~PORT_PCR_MUX_MASK;
     PORTB->PCR[1] = PORT_PCR_MUX(3); // Set PTB1 to TPM0_CH1
     TPM1_C1SC &= ~((TPM_CnSC_ELSB_MASK) | (TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSA_MASK));
     TPM1_C1SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));
-    TPM1_C1V = 0;
+    TPM1_C1V = 1;
 
     // Initialize PTB2 and PTB3 for motor direction control
     PORTB->PCR[2] &= ~PORT_PCR_MUX_MASK;
@@ -65,70 +65,71 @@ void motorControl(int state, int speed) {
     //speed should be between 0 to 100
     if (speed < 0) speed = 0;
     if (speed > 100) speed = 100;
-    uint16_t dutyCycle = (speed * 11999) / 100; // Convert speed to duty cycle
+    uint16_t dutyCycle = (speed * 4999) / 100; // Convert speed to duty cycle
 
     // Set PWM and direction based on motor state
     switch (state) {
         case STRAIGHT:
             TPM1_C0V = dutyCycle; // Motor 1
             TPM1_C1V = dutyCycle; // Motor 2
-            PTB->PSOR = (1 << 2); // PTB2 (AIN2) HIGH
-            PTB->PCOR = (1 << 3); // PTB3 (BIN2) LOW
+            PTB->PSOR |= (1 << 2); // PTB2 (AIN2) HIGH
+            PTB->PCOR |= (1 << 3); // PTB3 (BIN2) LOW
             break;
 
         case TURN_CLOCKWISE:
             TPM1_C0V = dutyCycle; // Motor 1
             TPM1_C1V = dutyCycle; // Motor 2
-            PTB->PSOR = (1 << 3); // PTB3 (BIN2) HIGH
-            PTB->PSOR = (1 << 2); // PTB2 (AIN2) HIGH
+            PTB->PSOR |= (1 << 3); // PTB3 (BIN2) HIGH
+            PTB->PSOR |= (1 << 2); // PTB2 (AIN2) HIGH
             break;
 
         case REVERSE:
             TPM1_C0V = dutyCycle; // Motor 1
             TPM1_C1V = dutyCycle; // Motor 2
-            PTB->PSOR = (1 << 3); // PTB3 (BIN2) HIGH
-            PTB->PCOR = (1 << 2); // PTB2 (AIN2) LOW
+            PTB->PSOR |= (1 << 3); // PTB3 (BIN2) HIGH
+            PTB->PCOR |= (1 << 2); // PTB2 (AIN2) LOW
             break;
 
         case TURN_ANTICLOCKWISE:
             TPM1_C0V = dutyCycle; // Motor 1
             TPM1_C1V = dutyCycle; // Motor 2
-            PTB->PCOR = (1 << 2); // PTB2 (AIN2) LOW
-            PTB->PCOR = (1 << 3); // PTB3 (BIN2) LOW
+            PTB->PCOR |= (1 << 2); // PTB2 (AIN2) LOW
+            PTB->PCOR |= (1 << 3); // PTB3 (BIN2) LOW
             break;
 
         case FORWARD_RIGHT_TURN:
             TPM1_C0V = dutyCycle; // Motor 1 (faster)
             TPM1_C1V = dutyCycle * 0.7; // Motor 2 (slower)
-            PTB->PSOR = (1 << 2); // PTB2 (AIN2) HIGH
-            PTB->PCOR = (1 << 3); // PTB3 (BIN2) LOW
+            PTB->PSOR |= (1 << 2); // PTB2 (AIN2) HIGH
+            PTB->PCOR |= (1 << 3); // PTB3 (BIN2) LOW
             break;
 
         case FORWARD_LEFT_TURN:
             TPM1_C0V = dutyCycle * 0.7; // Motor 1 (slower)
             TPM1_C1V = dutyCycle; // Motor 2 (faster)
-            PTB->PSOR = (1 << 2); // PTB2 (AIN2) HIGH
-            PTB->PCOR = (1 << 3); // PTB3 (BIN2) LOW
+            PTB->PSOR |= (1 << 2); // PTB2 (AIN2) HIGH
+            PTB->PCOR |= (1 << 3); // PTB3 (BIN2) LOW
             break;
 
         case BACKWARDS_LEFT_TURN:
             TPM1_C0V = dutyCycle * 0.7; // Motor 1 (slower)
             TPM1_C1V = dutyCycle; // Motor 2 (faster)
-            PTB->PSOR = (1 << 3); // PTB3 (BIN2) HIGH
-            PTB->PCOR = (1 << 2); // PTB2 (AIN2) LOW
+            PTB->PSOR |= (1 << 3); // PTB3 (BIN2) HIGH
+            PTB->PCOR |= (1 << 2); // PTB2 (AIN2) LOW
             break;
 
         case BACKWARDS_RIGHT_TURN:
             TPM1_C0V = dutyCycle; // Motor 1 (faster)
             TPM1_C1V = dutyCycle * 0.7; // Motor 2 (slower)
-            PTB->PSOR = (1 << 3); // PTB3 (BIN2) HIGH
-            PTB->PCOR = (1 << 2); // PTB2 (AIN2) LOW
+            PTB->PSOR |= (1 << 3); // PTB3 (BIN2) HIGH
+            PTB->PCOR |= (1 << 2); // PTB2 (AIN2) LOW
             break;
 
         default:
             // Stop motors if state is invalid
-            TPM1_C0V = 0; // Motor 1
-            TPM1_C1V = 0; // Motor 2
+						PTB->PCOR |= ((1 << 2) | (1 << 3));
+            TPM1_C0V = 1; // Motor 1
+            TPM1_C1V = 1; // Motor 2
             break;
     }
 }
