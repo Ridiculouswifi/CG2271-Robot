453009f3de596e4abd228852215a7a5af28d645e
Ridiculouswifi <thtan02@gmail.com>
2024-11-05 17:33:31 +0800

    Change motor direction

--- freedom_board/robotMotorControl.h
+++ freedom_board/robotMotorControl.h
@@ -83,10 +83,10 @@ void motorControl(int state, int speed) {
 	// Set PWM and direction based on motor state
 	switch (state) {
 	case STRAIGHT:
-		TPM1_C0V = dutyCycle;	// A1 IN
-		TPM1_C1V = 0; 			// A2 IN
-		TPM2_C0V = dutyCycle;	// B1 IN
-		TPM2_C1V = 0; 			// B2 IN
+		TPM1_C0V = 0; 			// A1 IN
+		TPM1_C1V = dutyCycle;	// A2 IN
+		TPM2_C0V = 0; 			// B1 IN
+		TPM2_C1V = dutyCycle;	// B2 IN
 		break;
 
 	case TURN_CLOCKWISE:
@@ -97,10 +97,10 @@ void motorControl(int state, int speed) {
 		break;
 
 	case REVERSE:
-		TPM1_C0V = 0; 			// A1 IN
-		TPM1_C1V = dutyCycle;	// A2 IN
-		TPM2_C0V = 0; 			// B1 IN
-		TPM2_C1V = dutyCycle;	// B2 IN
+		TPM1_C0V = dutyCycle;	// A1 IN
+		TPM1_C1V = 0; 			// A2 IN
+		TPM2_C0V = dutyCycle;	// B1 IN
+		TPM2_C1V = 0; 			// B2 IN
 		break;
 
 	case TURN_ANTICLOCKWISE:
@@ -111,31 +111,31 @@ void motorControl(int state, int speed) {
 		break;
 
 	case FORWARD_RIGHT_TURN:
-		TPM1_C0V = 0; //dutyCycle * 0.5; 		// A1 IN
-		TPM1_C1V = 0; 				// A2 IN
-		TPM2_C0V = dutyCycle; 		// B1 IN
-		TPM2_C1V = 0; 				// B2 IN
+		TPM1_C0V = 0; 				// A1 IN
+		TPM1_C1V = dutyCycle; 		// A2 IN
+		TPM2_C0V = 0; 				// B1 IN
+		TPM2_C1V = 0;				// B2 IN
 		break;
 
 	case FORWARD_LEFT_TURN:
-		TPM1_C0V = dutyCycle; 		// A1 IN
-		TPM1_C1V = 0; 				// A2 IN
-		TPM2_C0V = 0; //dutyCycle * 0.5; 		// B1 IN
-		TPM2_C1V = 0; 				// B2 IN
+		TPM1_C0V = 0; 				// A1 IN
+		TPM1_C1V = 0;		 		// A2 IN
+		TPM2_C0V = 0; 				// B1 IN
+		TPM2_C1V = dutyCycle; 		// B2 IN
 		break;
 
 	case BACKWARDS_LEFT_TURN:
 		TPM1_C0V = 0; 				// A1 IN
-		TPM1_C1V = dutyCycle; 		// A2 IN
-		TPM2_C0V = 0; 				// B1 IN
-		TPM2_C1V = 0; //dutyCycle * 0.5; 		// B2 IN
+		TPM1_C1V = 0;		 		// A2 IN
+		TPM2_C0V = dutyCycle; 		// B1 IN
+		TPM2_C1V = 0;				// B2 IN
 		break;
 
 	case BACKWARDS_RIGHT_TURN:
-		TPM1_C0V = 0; 				// A1 IN
-		TPM1_C1V = 0; //dutyCycle * 0.5; 		// A2 IN
-		TPM2_C0V = 0; 				// B1 IN
-		TPM2_C1V = dutyCycle; 		// B2 IN
+		TPM1_C0V = dutyCycle; 		// A1 IN
+		TPM1_C1V = 0; 				// A2 IN
+		TPM2_C0V = 0;               // B1 IN
+		TPM2_C1V = 0; 				// B2 IN
 		break;
 
 	default:
